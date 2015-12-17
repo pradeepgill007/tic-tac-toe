f0da992f65f4693fda167e22e7e53743632410b0
gurinder <gurinder@headerlabs.com>
2015-12-17 14:31:31 +0530

    changes in sign-in and sign-p html

--- www/js/app.js
+++ www/js/app.js
@@ -1,49 +1,49 @@
 // Ionic Starter App
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-  // the 2nd parameter is an array of 'requires'
-  angular.module('starter', ['ionic'])
-  .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-  if(window.cordova && window.cordova.plugins.Keyboard) {
-  // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-  // for form inputs)
-  cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-  // Don't remove this line unless you know what you are doing. It stops the viewport
-  // from snapping when text inputs are focused. Ionic handles this internally for
-  // a much nicer keyboard experience.
-  cordova.plugins.Keyboard.disableScroll(true);
-  }
-  if(window.StatusBar) {
-  StatusBar.styleDefault();
-  }
-  });
-  })
-  .config(function($stateProvider, $urlRouterProvider,$ionicConfigProvider) {
-$ionicConfigProvider.backButton.text('Go Back').icon('ion-chevron-left');
-$ionicConfigProvider.navBar.alignTitle("center"); //Places them at the bottom for all OS
-$ionicConfigProvider.tabs.position("bottom"); //Places them at the bottom for all OS
-  $urlRouterProvider.otherwise('/tab/home')
-  $stateProvider.state('tabs', {
-      url: "/tab",
-      abstract: true,
-      templateUrl: "template/tabs.html"
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic'])
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+                // for form inputs)
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                // Don't remove this line unless you know what you are doing. It stops the viewport
+                // from snapping when text inputs are focused. Ionic handles this internally for
+                // a much nicer keyboard experience.
+                cordova.plugins.Keyboard.disableScroll(true);
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
     })
-  .state('tabs.home', {
-      url: "/home",
-      views: {
-        'home-tab': {
-          templateUrl: "template/home.html"
-          // controller: 'HomeTabCtrl'
-        }
-      }
-    })
-    .state('tabs.signup', {
-      url: "/signup",
-      views: {
-        'signup-tab': {
-          templateUrl: "template/signup.html"
-        }
-      }
-    })
-});
+    .config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+        $ionicConfigProvider.backButton.text('Go Back').icon('ion-chevron-left');
+        $ionicConfigProvider.navBar.alignTitle("center"); //Places them at the bottom for all OS
+        $ionicConfigProvider.tabs.position("bottom"); //Places them at the bottom for all OS
+        $urlRouterProvider.otherwise('/tab/home')
+        $stateProvider.state('tabs', {
+            url: "/tab",
+            abstract: true,
+            templateUrl: "template/tabs.html"
+        })
+            .state('tabs.home', {
+                url: "/home",
+                views: {
+                    'home-tab': {
+                        templateUrl: "template/home.html"
+                        // controller: 'HomeTabCtrl'
+                    }
+                }
+            })
+            .state('tabs.signup', {
+                url: "/signup",
+                views: {
+                    'signup-tab': {
+                        templateUrl: "template/signup.html"
+                    }
+                }
+            })
+    });
